d9f8b1685addb2e788c8e39ab7bfbbe6aaec56cf
igorGomes <gomesoliveirasl02@hotmail.com>
2022-05-23 22:21:22 -0300

    parte visual pronta

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,69 @@
+import { CloseButton } from "../CloseButton";
+// import cactoImageUrl from '../assets/cacto.png'
+// import ideiaImageUrl from '../assets/lampada.png'
+// import otherImageUrl from '../assets/mundo.png'
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    //  Image:{
+    //    source:cactoImageUrl,
+    //    alt:'imagem de um cacto'
+    //  }
+  },
+  IDEIA: {
+    title: 'Ideia',
+    // Image:{
+    //   source:ideiaImageUrl,
+    //   alt:'imagem de uma lampada'
+    // }
+  },
+  OTHER: {
+    title: 'Outro',
+    //  Image:{
+    //   source:otherImageUrl,
+    //   alt:'imagem de um balão de planeta'
+    // }
+  },
+}
+export type FeedbackType = keyof typeof feedbackTypes;
+
+// Objetct.entries (feedbackTypes)
+
+// [
+//    [BUG,{...}],
+//    [IDEIA,...],
+//    [OTHER,...]
+// ]
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const[feedbackSent,setFeedbackSent] = useState(false)
+
+  function handleRestartFeedback(){
+    setFeedbackType(null)
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      
+      {/* key e value foram desestruturados, poderia ser assim (item) que pegaria o value completo do array */}
+     {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
+     ) : (
+       <FeedbackContentStep 
+       feedbackType={feedbackType}
+       onFeedbackRestartRequested = {handleRestartFeedback}
+       />
+     )}
+
+      <footer className="text-xs text-neutral-400">
+        feito com ♡. projeto da rockeseat
+      </footer>
+    </div>
+
+  )
+}
